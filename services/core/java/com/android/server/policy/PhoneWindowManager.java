c22fbc1e3668fca855cee258f6f64bcb4cbfa2df
LineageOS-Special-Project <yehonatan@mail.ru>
2025-01-22 05:38:15 +0000

    Fix compiling after latest merge

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -856,8 +856,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mLongSwipeDown;
     private CameraAvailbilityListener mCameraAvailabilityListener;
 
-    private CameraAvailbilityListener mCameraAvailabilityListener;
-
     private class PolicyHandler extends Handler {
 
         private PolicyHandler(Looper looper) {
